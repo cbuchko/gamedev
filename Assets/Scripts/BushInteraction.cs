7da3cb5911060af2be3ddd4327cabd50ff2a3248
Connor <connorbuchko2010@yahoo.ca>
2020-02-23 14:42:20 -0800

    added sprites and bush functionality

--- Assets/Scripts/BushInteraction.cs
+++ Assets/Scripts/BushInteraction.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BushInteraction : MonoBehaviour
 {
     private PlayerResources resources;
+    public int PickCount;
+    public bool Empty;
+    public Sprite FullSprite;
+    public Sprite EmptySprite;
     // Start is called before the first frame update
     void Start()
     {
         resources = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerResources>();
+        Button YesButton = GameObject.Find("YesButton").GetComponent<Button>();
+        YesButton.onClick.AddListener(() => RefillBush());
     }
 
     // Update is called once per frame
@@ -20,6 +27,24 @@ public class BushInteraction : MonoBehaviour
     /** Add a way to "Pick" the bush so that you can only take 
     one berry a day */
     public void DoInteraction(){
-        resources.resources["Food"] += 1;
+        if(!Empty){
+            resources.resources["Food"] += 1;
+            PickCount += 1;
+            if(PickCount >= 2){
+                EmptyBush();
+            }
+        }
+    }
+
+    public void EmptyBush(){
+        Empty = true;
+        GetComponent<SpriteRenderer>().sprite = EmptySprite;
+
+    }
+
+    public void RefillBush(){
+        Empty = false;
+        GetComponent<SpriteRenderer>().sprite = FullSprite;
+        PickCount = 0;
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gamedev
+
+TODO:
+
+Make resource panel and inventory slot only appear after you fill it
+
+Improve UI
+Make collecting resources satisfying by having the items drop.
+Make grass tileset.
+NPC Stuff after
+Fix bug that happens when spamming yes button
+
+COMPLETE:
+
+Make it so that only one or two berries can be picked a day 
+Food icon for resource panel
